931ae3c4174589698f7284ffdd956dd9479a1ec7
Nezred ✨ <101522353+nezred@users.noreply.github.com>
2024-12-26 00:43:59 +0100

    add custom path to dataset constructor

--- Progress_Task_2/mlflow/create_dataset.py
+++ Progress_Task_2/mlflow/create_dataset.py
@@ -1,13 +1,14 @@
 import pandas as pd
 
 class Dataset:
-    def __init__(self):
-        data = pd.read_csv("../data/df_encoded.csv")
-        target = ["h1n1_vaccine","seasonal_vaccine"]
+    def __init__(self, path:str = "../data/df_encoded.csv",target = ["h1n1_vaccine","seasonal_vaccine"] ):
+        data = pd.read_csv(path)
+        self.target = target
         data.set_index("respondent_id", inplace=True)
+        data.dropna(subset=['marital_status'],inplace=True)
         self._y = data[target]
         self._X = data.drop(columns=target)
-        test_data =  pd.read_csv("../data/test_encoded.csv")
+        test_data =  pd.read_csv(path)
         test_data.set_index("respondent_id", inplace=True)
         self.test = test_data
     
