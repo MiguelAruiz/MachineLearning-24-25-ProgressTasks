cb791a6b62b09bf96eef8c4f6ae815f32832637a
Elena Ballesteros Morallón <95474265+elena-17@users.noreply.github.com>
2024-12-26 17:41:30 +0000

    update main branch

--- Progress_Task_2/mlflow/create_dataset.py
+++ Progress_Task_2/mlflow/create_dataset.py
@@ -1,36 +1,61 @@
-import re
+
+
+######################################## IMPORTS ########################################
 import pandas as pd
+import configparser
 from sklearn.preprocessing import OneHotEncoder
 
+#########################################################################################
+
+
+################################## Global Configurations ################################
+# Load configuration fileconfig["data"]["targets"]
+CONFIG_FILE_PATH = "mlflow/test.conf"
+
+config = configparser.ConfigParser()
+config.read(CONFIG_FILE_PATH)
+DATASET_PATH = config["data"]["dataset_path"]
+TEST_DATASET_PATH = config["data"]["dataset_test_path"]
+print(DATASET_PATH)
+DATASET_INDEX_FEATURE = config["data"]["dataset_index"]
+DATASET_TARGET_FEATURES = ["h1n1_vaccine", "seasonal_vaccine"]
+
+#########################################################################################
 class Dataset:
+    '''
+    ## Dataset
+    
+    This class represents a dataset. It handles dataset loading and splitting.
+    
+    ### Attributes
+    
+    - test: The test dataset.
+    
+    '''
     def __init__(self):
-        data = pd.read_csv("../data/df_encoded.csv")
-        target = ["h1n1_vaccine","seasonal_vaccine"]
-        data.set_index("respondent_id", inplace=True)
+        '''
+        Constructor for the Dataset class.
+        '''
+        data = pd.read_csv(DATASET_PATH)
+        target = DATASET_TARGET_FEATURES
+        data.set_index(DATASET_INDEX_FEATURE, inplace=True)
         self._y = data[target]
         self._X = data.drop(columns=target)
-        test_data =  pd.read_csv("../data/test_encoded.csv")
-        test_data.set_index("respondent_id", inplace=True)
+        test_data =  pd.read_csv(TEST_DATASET_PATH)
+        test_data.set_index(DATASET_INDEX_FEATURE, inplace=True)
         self.test = test_data
     
     def with_correlation(self):
-        return self._X.copy(), self._y.copy(), self.test.copy()
-    
-    def filter(self):
-        X = pd.read_csv('../data/X_filtered.csv')
-        y = pd.read_csv('../data/y_filtered.csv')
-        X.set_index("respondent_id", inplace=True)
-        y.set_index("respondent_id", inplace=True)
-        return X, y
-    
-    def encoded(self):
-        X = pd.read_csv('../data/new.csv', index_col="respondent_id")
-        target = ["h1n1_vaccine","seasonal_vaccine"]
-        y = X[target]
-        X = X.drop(columns=target)
-        test = pd.read_csv('../data/new_test.csv', index_col="respondent_id")
-        return X, y, test
-    
+        '''
+        ## with_correlation
+        Method that returns a copy of the dataset features and targets.
+        
+        ### Returns
+        (X, y): A tuple containing the dataset features and targets.
+        '''
+        
+        return self._X.copy(), self._y.copy()
+
     def with_onehot(self):
         '''
         ## with_onehot
@@ -47,7 +72,44 @@ class Dataset:
         test_transformed = encoder.transform(self.test)
         test_df = pd.DataFrame(test_transformed.toarray(), index=self.test.index)
         return X, y, test_df
+    
+    def with_division(self):
+        ''''
+        ## with_division
+        Method that returns a tuple containing the dataset features and targets divided into two datasets, one for each target.
+        
+        ### Returns
+        ((h1n1, h1n1_y, test_h1n1), (seasonal, seasonal_y, test_seasonal)): A tuple containing two tuples, each containing the dataset features, targets and test dataset for each target.'''
+        
+        h1_n1 = self._X.copy()
+        seasonal = self._X.copy()
+        h1_columns = ['doctor_recc_seasonal','opinion_seas_vacc_effective','opinion_seas_risk','opinion_seas_sick_from_vacc']
+        seasonal_columns = ['h1n1_concern','h1n1_knowledge','doctor_recc_h1n1','opinion_h1n1_vacc_effective','opinion_h1n1_risk','opinion_h1n1_sick_from_vacc']
+        h1_n1.drop(columns=h1_columns, inplace=True)
+        seasonal.drop(columns=seasonal_columns, inplace=True)
+        h1_n1_y = self._y['h1n1_vaccine']
+        seasonal_y = self._y['seasonal_vaccine']
+        test_h1_n1 = self.test.copy()
+        test_seasonal = self.test.copy()
+        test_h1_n1.drop(columns=h1_columns, inplace=True)
+        test_seasonal.drop(columns=seasonal_columns, inplace=True)
+        return (h1_n1, h1_n1_y, test_h1_n1), (seasonal, seasonal_y, test_seasonal)
 
+    def all_features(self):
+        '''
+        ## all_features
+        Method that returns the dataset encoded with all the features without dropping the ones with random values.
+
+        ### Returns
+        X, y, test
+        '''
+        X = pd.read_csv(config["data"]["dataset_all_path"], index_col="respondent_id")
+        target = ["h1n1_vaccine","seasonal_vaccine"]
+        y = X[target]
+        X = X.drop(columns=target)
+        test = pd.read_csv(config["data"]["dataset_test_all_path"], index_col="respondent_id")
+        return X, y, test
+    
     def no_outliers(self):
         '''
         ## no_outliers
@@ -56,7 +118,7 @@ class Dataset:
         ### Returns
         X, y, test
         '''
-        X = pd.read_csv('../data/df_encoded_no_outliers.csv', index_col="respondent_id")
+        X = pd.read_csv(config["data"]["dataset_no_outliers_path"], index_col="respondent_id")
         target = ["h1n1_vaccine","seasonal_vaccine"]
         y = X[target]
         X = X.drop(columns=target)
@@ -69,7 +131,7 @@ class Dataset:
         ### Returns
         X, y, test
         '''
-        X = pd.read_csv('../data/df_encoded_no_outliers.csv', index_col="respondent_id")
+        X = pd.read_csv(config["data"]["dataset_no_outliers_path"], index_col="respondent_id")
         target = ["h1n1_vaccine","seasonal_vaccine"]
         y = X[target]
         X = X.drop(columns=target)
